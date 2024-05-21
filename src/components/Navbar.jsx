c3154dc92b0766cca0fd2e6899f4180e364859fc
Devanshu <bhattidevanshu71821@gmail.com>
2024-05-21 18:53:22 +0530

    update code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>      
+    </div>
+  )
+}
+
+export default Navbar
